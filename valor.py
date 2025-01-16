6f8b861f7054f32fd43b0e10282109ba208516b4
Rafael Dias Campos <73750287+RafaelDiasCmp@users.noreply.github.com>
2025-01-16 16:48:38 -0500

    segundo commit

---
+++ valor.py
@@ -0,0 +1,8 @@
+produto = 30
+valor = 0
+
+while valor < produto:
+    print("\nPrecisa de mais para comprar o produto!")
+    valor += int(input("Digite o valor: "))
+
+print("\nVocê tem o suficiente para comprar o produto!\n")
